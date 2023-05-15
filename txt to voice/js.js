ce5fea81a1fa2a8536f67a48000388688f3e2a0e
shafincode <133623067+shafincode@users.noreply.github.com>
2023-05-15 09:04:05 +0500

    Add files via upload

---
+++ txt to voice/js.js
@@ -0,0 +1,41 @@
+var txtInput = document.querySelector('#txtInput');
+var voiceList = document.querySelector('#voiceList');
+var btnSpeak = document.querySelector('#btnSpeak');
+var synth = window.speechSynthesis;
+var voices = [];
+var alifbo = `a,b,c,d,e,f,g,h,i,j,k,l,m,o,n,p,q,r,s,t,u,v,w,x,y,z`
+
+
+PopulateVoices();
+if (speechSynthesis !== undefined) {
+    speechSynthesis.onvoiceschanged = PopulateVoices;
+}
+
+btnSpeak.addEventListener('click', () => {
+
+
+    var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
+    var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+    voices.forEach((voice) => {
+        if (voice.name === selectedVoiceName) {
+            toSpeak.voice = voice;
+        }
+    });
+    synth.speak(toSpeak);
+
+});
+
+function PopulateVoices() {
+    voices = synth.getVoices();
+    var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+    voiceList.innerHTML = '';
+    voices.forEach((voice) => {
+        var listItem = document.createElement('option');
+        listItem.textContent = voice.name;
+        listItem.setAttribute('data-lang', voice.lang);
+        listItem.setAttribute('data-name', voice.name);
+        voiceList.appendChild(listItem);
+    });
+
+    voiceList.selectedIndex = selectedIndex;
+}
